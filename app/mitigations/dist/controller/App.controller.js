70dcfab8e4989c5b7a5ba8d7911b24777839c3f1
Shahrukh Bhat <shahrukh.bhat@sap.com>
2023-06-15 00:08:28 +0000

    added UI deploy config and app logging binding

---
+++ app/mitigations/dist/controller/App.controller.js
@@ -0,0 +1,1 @@
+sap.ui.define(["./BaseController"],function(t){"use strict";return t.extend("nssb.mitigations.controller.App",{onInit:function(){this.getView().addStyleClass(this.getOwnerComponent().getContentDensityClass())}})});
---
+++ app/risks/dist/Component.js
@@ -0,0 +1,1 @@
+sap.ui.define(["sap/fe/core/AppComponent"],function(e){"use strict";return e.extend("nssb.risks.Component",{metadata:{manifest:"json"}})});
